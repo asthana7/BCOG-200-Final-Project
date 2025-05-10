26d5fd0eb092bcdb287d7ce43f03b960e3aa943c
asthana7 <asthana7@illinois.edu>
2025-05-10 09:30:57 -0500

    changes to src\hand_model\model init

---
+++ src/hand_model/model/__init__.py
@@ -0,0 +1,2 @@
+from src.hand_model.model.keypoint_classifier.keypoint_classifier import KeyPointClassifier
+from src.hand_model.model.point_history_classifier.point_history_classifier import PointHistoryClassifier

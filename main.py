e13bd2ba451907dcf1ec5a698b3bdff8a3937ff0
asthana7 <asthana7@illinois.edu>
2025-05-10 09:00:49 -0500

    created main to run code

---
+++ main.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from src.potent_gui import AudioGestureApp
+
+def main():
+    root = tk.Tk()
+    app = AudioGestureApp(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
